dc708ce312ddc7c30cbaaa700d50cd2adc35a7ec
Melin Kan <melin.kan@outlook.com>
2024-03-17 14:34:49 +0300

    fix Wrapper props and styles

--- app/components/Wrapper/Wrapper.tsx
+++ app/components/Wrapper/Wrapper.tsx
@@ -1,9 +1,12 @@
+import React from "react";
 import styles from "./Wrapper.module.css";
 
 export function Wrapper({
   children,
+  className,
 }: Readonly<{
   children: React.ReactNode;
+  className?: string;
 }>) {
-  return <div className={styles.wrapper}>{children}</div>;
+  return <div className={`${styles.wrapper} ${className}`}>{children}</div>;
 }
