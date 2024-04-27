1b818ab7468aac70f728bc7fc004856130f98bea
Melin Kan <melin.kan@outlook.com>
2024-04-27 15:59:03 +0300

    add global img styles and new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { Syne, Inter } from "next/font/google";
 import StyledComponentsRegistry from "./lib/registry";
 import "./globals.css";
 
-const dm_sans = DM_Sans({ subsets: ["latin"] });
+export const syne = Syne({ subsets: ["latin"] });
+export const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Home - Melin Kan",
   description:
-    "Hello, I'm Melin Kan. A detail-oriented developer dedicated to crafting visually stunning, user-friendly websites while staying updated with the latest trends.",
+    "Meet Melin, a passionate web developer & designer with expertise in technical SEO and creating user-friendly, optimized websites. Always keeping up with the latest trends.",
 };
 
 export default function RootLayout({
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={dm_sans.className}>
+      <body className={syne.className}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
