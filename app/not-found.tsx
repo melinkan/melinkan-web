794b92e6908ef268199d8305eadbfbd1a1af86a8
Melin Kan <melin.kan@outlook.com>
2024-04-27 12:47:30 +0300

    remove Wrapper

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,22 @@
 import Link from "next/link";
 import { BiArrowBack } from "react-icons/bi";
 
-import { Wrapper } from "./components";
 import styles from "./page.module.css";
 
 export default function NotFound() {
   return (
     <main>
-      <Wrapper>
-        <div className={styles.notFound}>
-          <div>
-            <p className={styles.errorNo}>404</p>
-            <p className={styles.errorText}>Page Not Found</p>
-          </div>
-          <Link className={styles.button} href="/">
-            {" "}
-            <BiArrowBack />
-            Back to Home Page
-          </Link>
+      <div className={styles.notFound}>
+        <div>
+          <p className={styles.errorNo}>404</p>
+          <p className={styles.errorText}>Page Not Found</p>
         </div>
-      </Wrapper>
+        <Link className={styles.button} href="/">
+          {" "}
+          <BiArrowBack />
+          Back to Home Page
+        </Link>
+      </div>
     </main>
   );
 }
