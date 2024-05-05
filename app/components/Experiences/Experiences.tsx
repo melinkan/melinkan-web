8cc2805ac6e29fa27403cced39992041df6b17fe
Melin Kan <melin.kan@outlook.com>
2024-05-05 22:42:04 +0300

    add Experiences

---
+++ app/components/Experiences/Experiences.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import SectionTitle from "../styled/StyledSectionTitles";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+
+import {
+  StyledExperienceCard,
+  StyledExperienceContainer,
+  StyledExperienceDate,
+  StyledExperienceDescription,
+  StyledExperienceTitle,
+  StyledExperiencesWrapper,
+} from "../styled/StyledExperiences";
+
+export function Experiences() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const cardVariants = {
+    offscreen: { x: -100, opacity: 0 },
+    onscreen: {
+      x: isInView ? 0 : -100,
+      opacity: isInView ? 1 : 0,
+      transition: {
+        type: "spring",
+        duration: 0.8,
+        delay: 0.5,
+      },
+    },
+  };
+
+  return (
+    <StyledExperiencesWrapper>
+      <SectionTitle title="Work Experiences" />
+      <StyledExperienceContainer
+        initial="offscreen"
+        animate="onscreen"
+        exit="offscreen"
+        variants={cardVariants}
+        ref={ref}
+      >
+        <StyledExperienceCard>
+          <StyledExperienceDate>Oct 2022 - Current</StyledExperienceDate>
+          <StyledExperienceTitle>
+            Marketing Web Developer, <i>Commencis</i>
+          </StyledExperienceTitle>
+          <StyledExperienceDescription>
+            As a marketing web developer, I design, develop, and maintain
+            websites using WordPress, JavaScript, and CSS, while also supporting
+            our Web Frontend Team with ReactJS and TypeScript. My role includes
+            integrating analytics with tools like Google Tag Manager and Google
+            Analytics, and optimizing website performance through technical SEO.
+            Additionally, I create visually appealing emails using
+            ActiveCampaign, HTML, and CSS.
+          </StyledExperienceDescription>
+        </StyledExperienceCard>
+        <StyledExperienceCard>
+          <StyledExperienceDate>Jul 2022 - Sep 2022</StyledExperienceDate>
+          <StyledExperienceTitle>
+            Software Intern, <i>Hubtic</i>
+          </StyledExperienceTitle>
+          <StyledExperienceDescription>
+            During my tenure as an intern at Hubtic, I gained valuable
+            experience working on web projects utilizing React. Additionally, I
+            worked on a marketing website that utilized WordPress. This
+            experience allowed me to develop my skills in front-end development.
+          </StyledExperienceDescription>
+        </StyledExperienceCard>
+      </StyledExperienceContainer>
+    </StyledExperiencesWrapper>
+  );
+}
---
+++ app/components/styled/StyledExperiences.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+import Container from "./Container";
+
+export const StyledExperiencesWrapper = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid rgb(var(--clr-dark));
+
+  @media (max-width: 992px) {
+    flex-direction: column;
+    gap: 24px;
+  }
+`;
+
+export const StyledExperienceContainer = styled(motion.div)`
+  flex: 3;
+`;
+
+export const StyledExperienceCard = styled(motion.div)`
+  &:not(:last-child) {
+    margin-bottom: 36px;
+  }
+`;
+
+export const StyledExperienceDate = styled(motion.div)`
+  font-family: "Inter", sans-serif;
+  font-size: 16px;
+  font-weight: 300;
+  margin-bottom: 8px;
+`;
+
+export const StyledExperienceTitle = styled(motion.h3)`
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 8px;
+`;
+
+export const StyledExperienceDescription = styled(motion.p)`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.5;
+`;
