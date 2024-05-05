f10e75ca8b249e6e58e2c5c45aa4ea72575724d6
Melin Kan <melin.kan@outlook.com>
2024-05-05 21:49:08 +0300

    add TechStack

---
+++ app/components/TechStack/TechStack.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Marquee from "react-fast-marquee";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+
+import SectionTitle from "../styled/StyledSectionTitles";
+import {
+  StyledMarqueeMotion,
+  StyledMarqueeWrapper,
+  StyledTechStackWrapper,
+} from "../styled/StyledTechStack";
+
+import {
+  SiFigma,
+  SiFramer,
+  SiGoogleanalytics,
+  SiGoogletagmanager,
+  SiJavascript,
+  SiNextdotjs,
+  SiSemrush,
+  SiTypescript,
+  SiWebflow,
+  SiWordpress,
+} from "react-icons/si";
+import { IoLogoReact } from "react-icons/io5";
+
+export function TechStack() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const cardVariants = {
+    offscreen: { x: -100, opacity: 0 },
+    onscreen: {
+      x: isInView ? 0 : -100,
+      opacity: isInView ? 1 : 0,
+      transition: {
+        type: "spring",
+        duration: 0.8,
+        delay: 0.5,
+      },
+    },
+  };
+
+  return (
+    <StyledTechStackWrapper>
+      <SectionTitle title="Tech Stack" />
+      <StyledMarqueeMotion
+        initial="offscreen"
+        animate="onscreen"
+        exit="offscreen"
+        variants={cardVariants}
+        ref={ref}
+      >
+        <Marquee direction="right" speed={75}>
+          <StyledMarqueeWrapper>
+            <SiJavascript />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiTypescript />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <IoLogoReact />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiNextdotjs />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiWordpress />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiFigma />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiFramer />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiWebflow />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiSemrush />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiGoogletagmanager />
+          </StyledMarqueeWrapper>
+          <StyledMarqueeWrapper>
+            <SiGoogleanalytics />
+          </StyledMarqueeWrapper>
+        </Marquee>
+      </StyledMarqueeMotion>
+    </StyledTechStackWrapper>
+  );
+}
---
+++ app/components/styled/StyledTechStack.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import styled from "styled-components";
+import Container from "./Container";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export const StyledTechStackWrapper = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid rgb(var(--clr-dark));
+
+  @media (max-width: 992px) {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 24px;
+  }
+
+  & h2 {
+    flex: 2;
+  }
+`;
+
+export const StyledMarqueeMotion = styled(motion.div)`
+  flex: 3;
+  overflow: hidden;
+  width: 100%;
+`;
+
+export const StyledMarqueeWrapper = styled.div`
+  font-size: 40px;
+  margin: 0 36px;
+  text-align: center;
+`;
