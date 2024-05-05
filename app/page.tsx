6c00b41fe77426a007313aea1ec17fd8860232c5
Melin Kan <melin.kan@outlook.com>
2024-05-05 22:48:39 +0300

    add home page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import { HeaderHome } from "./components/Header/HeaderHome";
+import { WhatIDo } from "./components/WhatIDo/WhatIDo";
+import { TechStack } from "./components/TechStack/TechStack";
+import { Experiences } from "./components/Experiences/Experiences";
+import { Footer } from "./components/Footer/Footer";
 
 export default function Home() {
-  return <HeaderHome />;
+  return (
+    <>
+      <HeaderHome />
+      <WhatIDo />
+      <TechStack />
+      <Experiences />
+      <Footer />
+    </>
+  );
 }
